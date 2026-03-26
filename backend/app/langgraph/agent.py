52e62caba883fed0481b35a10c48aafc668656c4
muskan2320 <muskan1802.m@gmail.com>
2026-03-26 23:07:25 +0530

    Setup Groq LLM integration using LangChain and initialize LangGraph agent

---
+++ backend/app/langgraph/agent.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+if __name__ == "__main__":
+    response = llm.invoke("What is CRM?")
+    print(response.content)
