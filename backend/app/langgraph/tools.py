30b0bcc3612e97f9d6bbd18a2d1e0646545b6a3d
muskan2320 <muskan1802.m@gmail.com>
2026-03-27 22:43:11 +0530

    Add LogInteractionTool to enable AI-driven interaction logging via backend API

--- backend/app/langgraph/tools.py
+++ backend/app/langgraph/tools.py
@@ -15,6 +15,26 @@ def search_hcp_tool(name: str = None, hospital: str = None):
 
     return response.json()
 
+def log_interaction_tool(data: dict):
+    response = requests.post(f"{BASE_URL}/log-interaction", json=data)
+
+    return response.json()
+
 if __name__ == "__main__":
-    result = search_hcp_tool(name="Sharma")
+    # result = search_hcp_tool(name="Sharma")
+    # print(result)
+
+    sample_data = {
+        "doctor_name": "Dr Test",
+        "hospital": "Apollo",
+        "specialization": "Cardiology",
+        "city": "Delhi",
+        "interaction_date": "2026-03-20",
+        "topic": "Diabetes drug",
+        "follow_up_action": "Send samples",
+        "follow_up_date": "2026-03-25",
+        "notes": "Doctor interested"
+    }
+
+    result = log_interaction_tool(sample_data)
     print(result)
