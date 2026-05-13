743c798f4d0dbf78def4a5c347227f5c22772f6e
muskan2320 <muskan1802.m@gmail.com>
2026-05-13 14:16:28 +0530

    added table for storing user login data

--- backend/app/models.py
+++ backend/app/models.py
@@ -39,3 +39,14 @@ class Interaction(Base):
     created_at = Column(DateTime, default=datetime.utcnow)
 
     hcp = relationship("HCP", back_populates="interactions")
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    email = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+
+    role = Column(String, default="user")
+    created_at = Column(DateTime, default=datetime.utcnow)
