2a1dacdd0e21a942467bc6a07c95fb6c157d392e
muskan2320 <muskan1802.m@gmail.com>
2026-05-14 12:17:39 +0530

    added authentication file

---
+++ backend/app/auth.py
@@ -0,0 +1,11 @@
+from passlib.context import CryptoContext
+
+pwd_context = CryptoContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str):
+
+    return pwd_context.verify(plain_password, hashed_password)
