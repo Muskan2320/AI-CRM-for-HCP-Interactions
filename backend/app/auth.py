c957e96f01d8ca747cff235259a5df3a48782c7d
muskan2320 <muskan1802.m@gmail.com>
2026-05-17 12:39:20 +0530

    tested sign up route and fixed: incorrect library name

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,9 +1,9 @@
-from passlib.context import CryptoContext
+from passlib.context import CryptContext
 
-pwd_context = CryptoContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
-
+    
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str):
